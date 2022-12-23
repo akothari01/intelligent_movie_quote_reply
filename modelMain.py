53c0d9e26a9fbaf057d0b68e2134d0dcacf4dfbc
akothari01 <74020378+akothari01@users.noreply.github.com>
2022-12-23 17:21:02 -0400

    used the sentiment classification model
    combined with custom type detection

--- modelMain.py
+++ modelMain.py
@@ -1,3 +1,30 @@
 from transformers import pipeline
+import SentenceTypeDetect
+
+
 classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
-print(classifier("I hate you"))
+
+
+def getMaxSentiment(sentiment):
+    maxSentiment = ""
+    maxVal = -1.0
+    for ind in sentiment[0]:
+        if ind['score'] > maxVal:
+            maxSentiment = ind['label']
+            maxVal = ind['score']
+    return maxSentiment
+
+
+def sentimentAnalyzer(sentence):
+    sentiment = ""
+    typeS = "none"
+    if SentenceTypeDetect.isQuestion(sentence):
+        typeS = "question"
+    if SentenceTypeDetect.isGreeting(sentence):
+        typeS = "greeting"
+    if SentenceTypeDetect.isOrder(sentence):
+        typeS = "order"
+    sentiment = getMaxSentiment(classifier(sentence))
+    return sentiment, typeS
+
+
