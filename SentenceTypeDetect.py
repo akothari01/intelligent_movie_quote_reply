9070f397627895f51814c1852cd021ba30a1a905
akothari01 <74020378+akothari01@users.noreply.github.com>
2022-12-23 16:49:12 -0400

    wrote isOrder and isGreeting function
    similar to isQuestion

--- SentenceTypeDetect.py
+++ SentenceTypeDetect.py
@@ -1,13 +1,18 @@
 from nltk.tokenize import word_tokenize
+from nltk import bigrams
 import nltk
+
 
 # question starters: have you; have they; do you; will you; will it; is it; could you; could it;  would you;
 # would it; were you; are you; has it; has he; has she; can he; can it; can you; can she;
 # can anyone; has anyone; will anyone; could anyone;
 
+
 Ws = ['WDT', 'WP', 'WRB']
 VMs = ['MD', 'VB', 'VBD', 'VBZ']
-Ps = ['PRP', 'PRP$', 'anyone']
+Ps = ['PRP', 'PRP$']
+Hs = ['hi', 'hey', 'sup', 'wassup', 'yo']
+Os = ['should', 'must']
 
 
 def isQuestion(question):
@@ -19,8 +24,27 @@ def isQuestion(question):
         return True
     for i, q in enumerate(qPOS):
         if i < len(qPOS) - 1:
-            if qPOS[i][1] in VMs and qPOS[i+1][1] in Ps:
+            if qPOS[i][1] in VMs and (qPOS[i+1][1] in Ps or qPOS[i+1][0] == 'anyone'):
                 return True
     return False
 
+
+def isGreeting(greeting):
+    gTokens = word_tokenize(greeting)
+    if gTokens[0] in Hs:
+        return True
+    gBigram = list(bigrams(gTokens))
+    if gBigram[0] == ('whats', 'up'):
+        return True
+    return False
+
+
+def isOrder(order):
+    oTokens = word_tokenize(order)
+    oPOS = nltk.pos_tag(oTokens)
+    for i, q in enumerate(oPOS):
+        if i < len(oPOS) - 1:
+            if oPOS[i][1] in 'Ps' and (oPOS[i+1][0] in Os or oPOS[i+1][1] in VMs):
+                return True
+    return False
 
